b49f8e9fffbd53d0ed3aa65df67a75e94afbf367
Oleg Kainov <kainov.oleg@gmail.com>
2018-08-15 18:59:56 +0200

    add calculateSum function and tests

---
+++ index.js
@@ -0,0 +1,3 @@
+function calculateSum(a, b) {
+    return a + b;
+}
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,14 @@
+QUnit.module("QUnit is working");
+
 QUnit.test("true is true", function (assert) {
     assert.ok(true === true, "Passed!");
+});
+
+QUnit.module("calculateSum function");
+QUnit.test("0 + 0 = 0", function (assert) {
+    assert.equal(calculateSum(0, 0), 0, "Passed!");
+});
+
+QUnit.test("1 + 0 = 1", function (assert) {
+    assert.equal(calculateSum(1, 0), 1, "Passed!");
 });
